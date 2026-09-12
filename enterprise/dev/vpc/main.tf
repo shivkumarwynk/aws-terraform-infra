b50f2303b86f567c0e13cd14f3c0b512a71963eb
shivkumarwynk <94594488+shivkumarwynk@users.noreply.github.com>
2026-09-12 15:06:05 +0530

    Update main.tf

--- enterprise/dev/vpc/main.tf
+++ enterprise/dev/vpc/main.tf
@@ -122,9 +122,6 @@ module "vpc" {
       "to_port" : 0
     }
   ]
-  manage_default_route_table    = false
-  manage_default_security_group = false
-
   enable_dns_hostnames = true
   enable_dns_support   = true
 
