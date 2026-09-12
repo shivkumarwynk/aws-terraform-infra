0477bcc9fb3d2d6c4d5864ca46e894d5299b8910
shivkumarwynk <94594488+shivkumarwynk@users.noreply.github.com>
2026-09-13 00:42:37 +0530

    Update variables.tf

--- enterprise/dev/alb/variables.tf
+++ enterprise/dev/alb/variables.tf
@@ -67,7 +67,7 @@ variable "enable_internal_https" {
 variable "certificate_arn" {
   description = "ACM certificate ARN for HTTPS listeners. Leave empty to run HTTP only"
   type        = string
-  default     = ""
+  default     = "arn:aws:acm:ap-south-1:916951114449:certificate/d5950627-3a1c-450c-9a76-d0fb5cf9cc3c"
 }
 
 variable "ssl_policy" {
