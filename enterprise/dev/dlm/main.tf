a424bd850b385b62bfd70051755295d49860af3b
shivkumarwynk <94594488+shivkumarwynk@users.noreply.github.com>
2026-09-12 22:00:55 +0530

    Remove name attribute from EBS snapshot policy
    Removed the name attribute from the EBS snapshot policy resource.

--- enterprise/dev/dlm/main.tf
+++ enterprise/dev/dlm/main.tf
@@ -51,7 +51,6 @@ resource "aws_dlm_lifecycle_policy" "ebs" {
   description        = "${local.name}-${local.env} EBS snapshot policy"
   execution_role_arn = module.dlm_role.arn
   state              = "ENABLED"
-  name            = "${local.name}-${local.env}-dlm"
 
   policy_details {
     resource_types = ["VOLUME"]
